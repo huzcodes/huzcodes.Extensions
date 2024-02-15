fd2befab82e050c21572b30e574ab4f8d353bf52
huzcodes <huzcodes@outlook.com>
2024-02-16 00:16:42 +0400

    adding global exception handler implementation as extension method that handle the default exception to be thrown in case non of the huzcodes extensions plugin are thrown

---
+++ huzcodes.Extensions/Exceptions/CustomResultException.cs
@@ -0,0 +1,12 @@
+﻿namespace huzcodes.Extensions.Exceptions
+{
+    public class CustomResultException<TException> : Exception where TException : class
+    {
+        public TException CustomExceptionContract { get; set; }
+
+        public CustomResultException(TException customExceptionContract)
+        {
+            CustomExceptionContract = customExceptionContract;
+        }
+    }
+}
