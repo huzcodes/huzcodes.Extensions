25ff5c4832d60a0b26e5da124f68e4e471ec8b6f
huzcodes <huzcodes@outlook.com>
2024-02-16 00:19:36 +0400

    adding two custom exception classes one that take only message and status code in its ctor, and the other one it take a generic type class in its ctor in case of custom model exception that needs to be thrown as per any business implementation and the default status code of it going to be 417 Expectation Failed

---
+++ huzcodes.Extensions/Exceptions/ResultException.cs
@@ -0,0 +1,14 @@
+﻿namespace huzcodes.Extensions.Exceptions
+{
+    public class ResultException : Exception
+    {
+        public string ResultExceptionMessage { get; set; } = string.Empty;
+        public int ResultExceptionStatusCode { get; set; }
+
+        public ResultException(string message, int statusCode)
+        {
+            ResultExceptionMessage = message;
+            ResultExceptionStatusCode = statusCode;
+        }
+    }
+}
