9cfdfb9b03090651577492a2b4594bfacb2e0300
huzcodes <huzcodes@outlook.com>
2024-02-17 14:11:59 +0400

    adding summery description for result exception contracts

--- huzcodes.Extensions/Exceptions/CustomResultException.cs
+++ huzcodes.Extensions/Exceptions/CustomResultException.cs
@@ -4,6 +4,11 @@
     {
         public TException CustomExceptionContract { get; set; }
 
+        /// <summary>
+        /// this class ctor, is used while you are thrown CustomResultException to be handled in the global exception handler,
+        /// it has one argument which is generic for any strcuture error or exception response you want to send.
+        /// </summary>
+        /// <param name="customExceptionContract"></param>
         public CustomResultException(TException customExceptionContract)
         {
             CustomExceptionContract = customExceptionContract;
--- huzcodes.Extensions/Exceptions/ResultException.cs
+++ huzcodes.Extensions/Exceptions/ResultException.cs
@@ -4,7 +4,13 @@
     {
         public string ResultExceptionMessage { get; set; } = string.Empty;
         public int ResultExceptionStatusCode { get; set; }
-
+        /// <summary>
+        /// this class ctor, is used while you are thrown ResultException to be handled in the global exception handler
+        /// it has two arguments one for the custom message you want to send in your response in case of errors or exceptions
+        /// the other one is the status code you want to send in your response in case of errors or exceptions.
+        /// </summary>
+        /// <param name="message">the message you want to send in your response in case of errors or exceptions.</param>
+        /// <param name="statusCode">the status code you want to send in your response in case of errors or exceptions.</param>
         public ResultException(string message, int statusCode)
         {
             ResultExceptionMessage = message;
