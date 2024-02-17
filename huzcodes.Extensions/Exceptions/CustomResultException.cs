696ddefd2233e03af7ba1e5967a6d3a1aee89e77
huzcodes <huzcodes@outlook.com>
2024-02-17 16:14:04 +0400

    updating the custom result exception class by removing the generic type and making it dynamic so we can cast it in the handler for any type without having the issue of must provide compile time type

--- huzcodes.Extensions/Exceptions/CustomResultException.cs
+++ huzcodes.Extensions/Exceptions/CustomResultException.cs
@@ -1,15 +1,15 @@
 ﻿namespace huzcodes.Extensions.Exceptions
 {
-    public class CustomResultException<TException> : Exception where TException : class
+    public class CustomResultException : Exception
     {
-        public TException CustomExceptionContract { get; set; }
+        public dynamic CustomExceptionContract { get; set; }
 
         /// <summary>
         /// this class ctor, is used while you are thrown CustomResultException to be handled in the global exception handler,
         /// it has one argument which is generic for any strcuture error or exception response you want to send.
         /// </summary>
         /// <param name="customExceptionContract"></param>
-        public CustomResultException(TException customExceptionContract)
+        public CustomResultException(dynamic customExceptionContract)
         {
             CustomExceptionContract = customExceptionContract;
         }
