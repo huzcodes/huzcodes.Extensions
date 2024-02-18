824445caf36ed45e0b9b48d6e582730465880823
huzcodes <huzcodes@outlook.com>
2024-02-18 18:11:47 +0400

    adding registration extension caller

--- huzcodes.Extensions/Exceptions/Handlers.cs
+++ huzcodes.Extensions/Exceptions/Handlers.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 using System.Text.Json;
 
@@ -8,6 +10,10 @@ namespace huzcodes.Extensions.Exceptions
 {
     public static class Handlers
     {
+        /// <summary>
+        /// Registring exception handler of the plugin, 
+        /// for handling any exception and thrown errors and custom responses, including fluent validation
+        /// </summary>
         public static void AddExceptionHandlerExtension(this IApplicationBuilder app)
         {
             app.UseExceptionHandler(o =>
@@ -47,10 +53,34 @@ namespace huzcodes.Extensions.Exceptions
                     context.Response.StatusCode = (int)HttpStatusCode.ExpectationFailed;
                 }
 
+                // handling for fluent validation
+                if (exception is FluentValidation.ValidationException validationException)
+                {
+                    var errors = validationException.Errors.Select(error => new
+                    {
+                        Property = error.PropertyName,
+                        Message = error.ErrorMessage
+                    });
+
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                    errorContent = JsonSerializer.Serialize(errors);
+                }
+
 
                 context.Response.ContentType = "application/json";
                 await context.Response.WriteAsync(errorContent, System.Text.Encoding.UTF8);
             }));
+        }
+
+        /// <summary>
+        /// Registring fluent validation in extension plugin, 
+        /// in case you want to use fluent validation in your solution,
+        /// and get handled in the exception handler of the plugin.
+        /// </summary>
+        /// <param name="contextType"></param>
+        public static void AddFluentValidation(this IServiceCollection services, Type contextType)
+        {
+            services.AddValidatorsFromAssembly(contextType.Assembly);
         }
     }
 }
