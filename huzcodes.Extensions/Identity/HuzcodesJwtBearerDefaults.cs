d3892b182d3a06bb4d268babe6090ca6885982c7
huzcodes <huzcodes@outlook.com>
2024-06-24 05:26:46 +0400

    - Adding custom jwt bearer token options class for the custom authorization handler.

---
+++ huzcodes.Extensions/Identity/HuzcodesJwtBearerDefaults.cs
@@ -0,0 +1,14 @@
+﻿namespace huzcodes.Extensions.Identity
+{
+    /// <summary>
+    /// Default values used by <see cref="HuzcodesAuthZHandler"/> for JWT bearer authentication.
+    /// </summary>
+    public static class HuzcodesJwtBearerDefaults
+    {
+        /// <summary>
+        /// Default value for AuthenticationScheme property.
+        /// </summary>
+        public const string AuthenticationScheme = "HuzcodesScheme";
+    }
+
+}
