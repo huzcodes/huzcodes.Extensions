3caaf63af332d0f57d8a654219afc04d1698fb14
huzcodes <huzcodes@outlook.com>
2024-06-24 05:28:48 +0400

    - Adding registration class for huzcodes identity extension plugin, for registring the services, implementations and middleware.

--- huzcodes.Extensions/Identity/IdentityRegistrations.cs
+++ huzcodes.Extensions/Identity/IdentityRegistrations.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 
 namespace huzcodes.Extensions.Identity
@@ -9,31 +12,58 @@ namespace huzcodes.Extensions.Identity
     public static class IdentityRegistrations
     {
         /// <summary>
-        /// Adding registration for Authorization for JWT Bearer
+        /// Adding registration for JWT Authorization with scheme Bearer.
+        /// Adding registrations for huzcodes identity extension services.
         /// </summary>
         /// <param name="SigningKey">The Key that used for Signing the token and passed here to validate it.</param>
         /// <returns></returns>
-        public static IServiceCollection AddAuthZ(this IServiceCollection services, string SigningKey) 
+        public static IServiceCollection AddAuthZ(this IServiceCollection services, string SigningKey)
         {
+            // adding registrations for huzcodes identity extension services
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<JwtSecurityTokenHandler>();
+            services.AddScoped<IIdentityManager, IdentityServices>();
+
+            // adding registration for jwt token authorizations
             var oKey = Encoding.ASCII.GetBytes(SigningKey);
             var oKeySym256 = new SymmetricSecurityKey(oKey);
+
             services.AddAuthentication(options =>
             {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = oKeySym256,
-                    ValidateLifetime = true,
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
+                options.DefaultAuthenticateScheme = HuzcodesJwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = HuzcodesJwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = HuzcodesJwtBearerDefaults.AuthenticationScheme;
+            }).AddScheme<AuthenticationSchemeOptions, HuzcodesAuthZHandler>(HuzcodesJwtBearerDefaults.AuthenticationScheme, options => { })
+              .AddJwtBearer(options =>
+              {
+                  options.SaveToken = true;
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {
+                      ValidateIssuerSigningKey = true,
+                      IssuerSigningKey = oKeySym256,
+                      ValidateLifetime = true,
+                      ValidateIssuer = false,
+                      ValidateAudience = false
+                  };
+
+                  options.Events = new JwtBearerEvents
+                  {
+                      OnMessageReceived = context =>
+                      {
+                          var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+                          if (string.IsNullOrEmpty(token))
+                          {
+                              context.NoResult();
+                          }
+                          return Task.CompletedTask;
+                      },
+                      OnAuthenticationFailed = context =>
+                      {
+                          context.NoResult();
+                          return Task.CompletedTask;
+                      }
+                  };
+              });
 
             return services;
         }
@@ -44,9 +74,9 @@ namespace huzcodes.Extensions.Identity
         /// </summary>
         /// <param name="app"></param>
         /// <returns></returns>
-        public static IApplicationBuilder AddIdentityAPIKeyMiddleWare(this IApplicationBuilder app) 
+        public static IApplicationBuilder AddAuthZMiddleWare(this IApplicationBuilder app)
         {
-            app.UseMiddleware<IdentityKeyMiddleware>();
+            app.UseMiddleware<IdentityMiddleware>();
             return app;
         }
     }
