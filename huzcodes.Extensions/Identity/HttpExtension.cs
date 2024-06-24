1a5a8a185da3394f57fda3a1968990b38b4986bc
huzcodes <huzcodes@outlook.com>
2024-06-24 05:25:10 +0400

    - Adding Http Extension function for reading token from header.

---
+++ huzcodes.Extensions/Identity/HttpExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace huzcodes.Extensions.Identity
+{
+    public static class HttpExtension
+    {
+        public static string GetAuthorizationHeader(this IHttpContextAccessor contextAccessor)
+        {
+            contextAccessor!.HttpContext!.Request.Headers.TryGetValue("Authorization", out var authorizationHeader);
+            if (authorizationHeader.ToString().Contains("Bearer"))
+                authorizationHeader = authorizationHeader.ToString()
+                                                         .Replace("Bearer", "")
+                                                         .TrimStart();
+
+            return authorizationHeader!;
+        }
+    }
+}
